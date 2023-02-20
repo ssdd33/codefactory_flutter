0012f43efbcc531f7d5c4cc89839d083e5a1b3d6
ssdd33 <beeblu.0@gmail.com>
2023-02-20 20:03:19 +0900

    dDayCalculator

---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  DateTime firstDay = DateTime.now();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.pink[200],
+        body: SafeArea(
+          top: true,
+          bottom: true,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              _DDay(onHeartPressed: onHeartPressed, firstDay: firstDay),
+              const _CoupleImage(),
+            ],
+          ),
+        ));
+  }
+
+  void onHeartPressed() {
+    showCupertinoDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            color: Colors.white,
+            height: 300,
+            child: CupertinoDatePicker(
+              maximumDate: DateTime.now().add(const Duration(hours: 1)),
+              mode: CupertinoDatePickerMode.date,
+              onDateTimeChanged: (DateTime date) {
+                setState(() {
+                  firstDay = date;
+                });
+              },
+            ),
+          ),
+        );
+      },
+      barrierDismissible: true,
+    );
+  }
+}
+
+class _DDay extends StatelessWidget {
+  final GestureTapCallback onHeartPressed;
+  final DateTime firstDay;
+
+  const _DDay({required this.onHeartPressed, required this.firstDay});
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+    final now = DateTime.now();
+
+    return Column(
+      children: [
+        const SizedBox(height: 16.0),
+        Text(
+          'U&I',
+          style: textTheme.displayLarge,
+        ),
+        const SizedBox(height: 16.0),
+        Text(
+          'the day we first met',
+          style: textTheme.bodyText1,
+        ),
+        Text(
+          '${firstDay.year}.${firstDay.month}.${firstDay.day}',
+          style: textTheme.bodyText2,
+        ),
+        const SizedBox(height: 16.0),
+        IconButton(
+          iconSize: 60.0,
+          onPressed: onHeartPressed,
+          icon: const Icon(Icons.favorite, color: Colors.red),
+        ),
+        const SizedBox(height: 16.0),
+        Text(
+            'D+${DateTime(now.year, now.month, now.day).difference(firstDay).inDays + 1}',
+            style: textTheme.displayMedium)
+      ],
+    );
+  }
+}
+
+class _CoupleImage extends StatelessWidget {
+  const _CoupleImage();
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Center(
+        child: Image.asset(
+          'asset/img/middle_image.png',
+          height: MediaQuery.of(context).size.height / 2,
+        ),
+      ),
+    );
+  }
+}
